748e8fd6e623fc8bbeec50c7c98332d8414bd140
attisch82 <attila.farkas.sd@gmail.com>
2025-06-08 17:09:30 +0200

    Creates DataError sealed interface to specify error types for local and remote errors

--- shared/src/commonMain/kotlin/com/attila/mycryptotrader/core/domain/DataError.kt
+++ shared/src/commonMain/kotlin/com/attila/mycryptotrader/core/domain/DataError.kt
@@ -1,4 +1,18 @@
 package com.attila.mycryptotrader.core.domain
 
-interface DataError {
+sealed interface DataError : Error {
+    enum class Remote : DataError {
+        TOO_MANY_REQUESTS,
+        REQUEST_TIMEOUT,
+        SERVER_ERROR,
+        UNKNOWN_ERROR,
+        SERIALIZATION_ERROR,
+        NO_INTERNET_CONNECTION
+    }
+
+    enum class Local : DataError {
+        DISK_FULL,
+        INSUFFICIENT_FUNDS,
+        UNKNOWN_ERROR
+    }
 }
